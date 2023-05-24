ad914d2c983b236a08c53ffc7e0dbff9abc94d4a
javabean7 <java.c@live.com>
2023-05-24 21:37:00 +0800

    2 files modified

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './style.css';
-import './code1';
+import res from './code1';
 
 export default function App() {
   return (
     <div>
       <h1>Hello StackBlitz!</h1>
       <p>Start editing to see some magic happen :)</p>
+      {res.toString()}
     </div>
   );
 }
